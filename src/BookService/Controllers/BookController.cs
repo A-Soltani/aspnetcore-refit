c6da2a2298102ad56ea39b6eaa68ebfbae29bb21
Ali Soltani <soltani1384@gmail.com>
2020-12-09 14:47:46 +0330

    adding book controller

---
+++ src/BookService/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookService.Controllers
+{
+    public class BookController: ControllerBase
+    {
+        [HttpGet("books")]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            var books = new List<Book>();
+
+            return Ok(books);
+        }
+    }
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
